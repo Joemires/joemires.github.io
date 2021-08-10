b236223783f1403f4e1ab9c5fa14a74f35fd6a4e
Joseph Bassey <joemires@localhost.localdomain>
2021-08-10 11:07:22 +0100

    Updated my resume to something sweet

--- js/contact.js
+++ js/contact.js
@@ -1,46 +1,56 @@
-$(function () {
-    "use strict";
-
-    // init the validator
-    // validator files are included in the download package
-    // otherwise download from http://1000hz.github.io/bootstrap-validator
-
-    $('#contact-form').validator();
-
-
-    // when the form is submitted
-    $('#contact-form').on('submit', function (e) {
-
-        // if the validator does not prevent form submit
-        if (!e.isDefaultPrevented()) {
-            var url = "form/contact.php";
-
-            // POST values in the background the the script URL
-            $.ajax({
-                type: "POST",
-                url: url,
-                data: $(this).serialize(),
-                success: function (data)
-                {
-                    // data = JSON object that contact.php returns
-
-                    // we recieve the type of the message: success x danger and apply it to the 
-                    var messageAlert = 'alert-' + data.type;
-                    var messageText = data.message;
-
-                    // let's compose Bootstrap alert box HTML
-                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-                    
-                    // If we have messageAlert and messageText
-                    if (messageAlert && messageText) {
-                        // inject the alert to .messages div in our form
-                        $('#contact-form').find('.messages').html(alertBox);
-                        // empty the form
-                        $('#contact-form')[0].reset();
-                    }
-                }
-            });
-            return false;
+// Contact Form
+function validateForm() {
+    var name = document.forms["myForm"]["name"].value;
+    var email = document.forms["myForm"]["email"].value;
+    var subject = document.forms["myForm"]["subject"].value;
+    var comments = document.forms["myForm"]["comments"].value;
+    document.getElementById("error-msg").style.opacity = 0;
+    document.getElementById('error-msg').innerHTML = "";
+    if (name == "" || name == null) {
+        document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Name*</div>";
+        fadeIn();
+        return false;
+    }
+    if (email == "" || email == null) {
+        document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Email*</div>";
+        fadeIn();
+        return false;
+    }
+    if (subject == "" || subject == null) {
+        document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Subject*</div>";
+        fadeIn();
+        return false;
+    }
+    if (comments == "" || comments == null) {
+        document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Comments*</div>";
+        fadeIn();
+        return false;
+    }
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("simple-msg").innerHTML = this.responseText;
+            document.forms["myForm"]["name"].value = "";
+            document.forms["myForm"]["email"].value = "";
+            document.forms["myForm"]["subject"].value = "";
+            document.forms["myForm"]["comments"].value = "";
         }
-    })
-});
+    };
+    xhttp.open("POST", "php/contact.php", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("name=" + name + "&email=" + email + "&subject=" + subject + "&comments=" + comments);
+    return false;
+}
+
+function fadeIn() {
+    var fade = document.getElementById("error-msg");
+    var opacity = 0;
+    var intervalID = setInterval(function() {
+        if (opacity < 1) {
+            opacity = opacity + 0.5
+            fade.style.opacity = opacity;
+        } else {
+            clearInterval(intervalID);
+        }
+    }, 200);
+}
